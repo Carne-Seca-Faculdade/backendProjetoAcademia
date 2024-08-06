195d250e0ad5ac5f7390bfc94244ae00e76b922b
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-08-06 20:06:24 -0300

    Created base package

---
+++ src/test/java/com/carneseca/app_academia/AppAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carneseca.app_academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
