bb2d385bf715cc922f91b67a9b7fc757c3269c43
LilianCAstanha <lilian_castanha@hotmail.com>
2024-09-17 07:49:24 -0300

    Update Entities Usuário

--- src/main/java/com/carneseca/app_academia/entities/Usuario.java
+++ src/main/java/com/carneseca/app_academia/entities/Usuario.java
@@ -1,4 +1,30 @@
 package com.carneseca.app_academia.entities;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Entity
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String nome;
+    private String email;
+    private String senha;
+    private Date dataNascimento;
+    private String genero;
+    private float peso;
+    private float altura;
+
+
+
 }
